a73b8d68facca65df5784cc485da06c0d7f321f8
Muhammad-Wudaid <wudaidgenius@gmail.com>
2022-11-22 10:27:01 +0400

    Exercise 2
    Movie tickets

--- Chapter 6- Loops/Exercises/Exercise 2.py
+++ Chapter 6- Loops/Exercises/Exercise 2.py
@@ -1,1 +1,13 @@
 prompt = "What is your age?"
+
+
+while True:
+    age = input(prompt)
+    age = int(age)
+
+    if age < 3:
+        print("Your entry fee is free")
+    elif age < 13:
+        print("Your entry fee is $10.")
+    else:
+        print("Your entry fee is $15.")
