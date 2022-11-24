4cf143cb2d1a0d6400d6553aca6659a2c0459e98
Muhammad-Wudaid <wudaidgenius@gmail.com>
2022-11-24 19:53:48 +0400

    Exercise 2

--- Chapter 2- Variables & Comments/Exercises/Exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -1,1 +1,1 @@
-print('Nelson mandela, "The greatest glory in living lies not in never falling, but in rising every time we fall."')
+print('Nelson mandela once said, "The greatest glory in living lies not in never falling, but in rising every time we fall."')
