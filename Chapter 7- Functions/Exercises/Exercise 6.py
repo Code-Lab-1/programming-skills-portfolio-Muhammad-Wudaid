36086398781d1fa1c45d9438b166f9e011dea3c4
Muhammad-Wudaid <wudaidgenius@gmail.com>
2022-11-24 14:14:10 +0400

    Exercise 6
    first and last name

---
+++ Chapter 7- Functions/Exercises/Exercise 6.py
@@ -0,0 +1,11 @@
+def name(first,last):
+    print(first+last)
+name("Muhammad", " Wudaid")
+
+def sum(first,last):
+    print(first+last)
+sum(4,6) 
+
+def sum(first,second,third,fourth):
+    print(first+second+third+fourth)
+sum(4,6,1,1) 
